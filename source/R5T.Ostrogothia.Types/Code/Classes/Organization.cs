1ad30d420a3c038ced0946e2d7ad35a414d70e13
David Coats <david@getsafetycone.com>
2021-05-03 14:39:14 -0700

    Added new R5T.Ostrogothia.Abstractions, R5T.Ostrogothia.Types, and R5T.Ostrogothia.Base libraries.

---
+++ source/R5T.Ostrogothia.Types/Code/Classes/Organization.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace R5T.Ostrogothia
+{
+    public class Organization : IOrganization
+    {
+        public string Name { get; set; }
+
+
+        public Organization()
+        {
+        }
+
+        public Organization(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
